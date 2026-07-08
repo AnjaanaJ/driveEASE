1ca63003c8a5e8f88df64a46865618da770c4ee8
AnjaanaJ <anjanajanani6@gmail.com>
2026-07-08 20:53:14 +0530

    Initialize react client with vite and tailwind css

---
+++ client/vite.config.js
@@ -0,0 +1,11 @@
+//configuring tailwind css
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+})

164fc7bb4024a6d4e4f89f18b1c645e3f6d20c35
AnjaanaJ <anjanajanani6@gmail.com>
2026-07-21 09:38:05 +0530

    [M1] feat: add logout and change password endpoints

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const { registerUser, loginUser } = require('../controllers/auth.controller');
+const { registerUser, loginUser ,logoutUser,changePassword} = require('../controllers/auth.controller');
 const verifyToken = require('../middleware/verifyToken');
 const requireRole=require('../middleware/requireRole');
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
+router.post('/logout',verifyToken,logoutUser);
+router.put('/change-password',verifyToken,changePassword);
 
 router.get('/me', verifyToken, (req, res) => {
   res.status(200).json({ message: 'Token is valid!', user: req.user });

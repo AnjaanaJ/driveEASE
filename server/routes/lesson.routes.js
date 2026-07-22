7f8f31a8cc1161a73c2d7f14f86c1434b3ee1fab
achiniThes <achini.thesanya@gmail.com>
2026-07-23 03:48:51 +0530

    [M4] feat:add conflictChecker middleware for booking overlap detection,tested with 3 scenarios in Postman(fresh booking,overlap conflict,back-to-back booking)

--- server/routes/lesson.routes.js
+++ server/routes/lesson.routes.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 const { createLesson,getLessons,getLessonById } = require('../controllers/lesson.controller');
 const verifyToken = require('../middleware/verifyToken');
+const conflictChecker = require('../middleware/conflictChecker');
 
-router.post('/', verifyToken, createLesson);
+
+router.post('/', verifyToken, conflictChecker, createLesson);
 router.get('/', verifyToken, getLessons);
 router.get('/:id', verifyToken, getLessonById);
 module.exports = router;

3306a0ed752d6528f7387f6332ee981f11f37c31
Jenifer226 <antonymelcom077@gmail.com>
2026-07-23 02:02:54 +0530

    [M3] feat: add instructor create API

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const authRoutes = require("./routes/auth.routes");
 const adminRoutes=require('./routes/admin.routes');
+const instructorRoutes = require("./routes/instructor.routes");
 const lessonRoutes = require("./routes/lesson.routes");
 const notificationRoutes = require("./routes/notification.routes");
 
@@ -17,6 +18,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/instructors", instructorRoutes);
 app.use("/api/admin", adminRoutes);
 app.use("/api/lessons", lessonRoutes);
 app.use("/api/notifications", notificationRoutes);

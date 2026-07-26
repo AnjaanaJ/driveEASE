0e7e0170066783ba419530edf3a663d33419533d
AnjaanaJ <anjanajanani6@gmail.com>
2026-07-27 00:52:53 +0530

    [M1] feat: add footer to the dashboard layout

--- client/src/layouts/DashboardLayout.jsx
+++ client/src/layouts/DashboardLayout.jsx
@@ -1,14 +1,16 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../components/shared/Footer"
 
 function DashboardLayout() {
   return (
-    <div className="flex">
+    <div className="min-h-screen flex flex-col">
       {/* Sidebar will go here in the next step */}
-      <div className="flex-1">
+      <div className="flex-1 flex-col">
         {/* Navbar will go here too */}
-        <main>
+        <main className="flex-1">
           <Outlet />
         </main>
+        <Footer/>
       </div>
     </div>
   );
